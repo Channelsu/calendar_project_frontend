be591fb1b9d9bf93dab333e9276be4520cccf11b
Channelsu <h_sugimoto0401@yahoo.co.jp>
2020-11-23 10:02:00 +0900

    スケジュールバーの色選択下地作成

--- pages/settings.vue
+++ pages/settings.vue
@@ -2,7 +2,7 @@
   <v-row justify="center" align="center">
     <v-col cols="12">
       <p>設定画面</p>
-      <p>名前変更、パスワード変更、スケジュールバーの色等の変更が可能</p>
+      <p>名前変更、パスワード変更等の変更が可能</p>
       <p>data-tableかsimple-tableかcardを使用</p>
     </v-col>
   </v-row>
