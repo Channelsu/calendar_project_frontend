7f04ed4defcf924eb82a0cfa8ac08f3680ec19d2
Channelsu <h_sugimoto0401@yahoo.co.jp>
2020-12-13 13:26:22 +0900

    日付の値を整形するグローバル関数の作成

--- plugins/util.js
+++ plugins/util.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 
+// 日付の値からハイフンがあれば削除。なければ挿入する。
+// 例1：'2020-12-15' ⇒ '20201215' 例2：'20201230' ⇒ '2020-12-30'
+Vue.prototype.$fmtDate = (date) => {
+  return date.includes('-')
+    ? date.replace(/-/g, '')
+    : `${date.substr(0, 4)}-${date.substr(4, 2)}-${date.substr(6, 2)}`
+}
+
 // 時間の値からコロンがあれば削除。なければ挿入する。
 // 例1：'13:15' ⇒ '1315' 例2：'1315' ⇒ '13:15'
 Vue.prototype.$fmtTime = (time) => {
