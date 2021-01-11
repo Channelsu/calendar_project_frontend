f29263e9bd9978197743de70600692e524b54824
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-01-11 18:24:55 +0900

    管理画面土台追加

---
+++ pages/management.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12">
+      <p>管理画面</p>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  // ヘッダーの種類記述（指定がない時はlayouts/default.vueが適応される）
+  layout: 'hd_general',
+
+  data() {
+    return {}
+  },
+  methods: {},
+}
+</script>
