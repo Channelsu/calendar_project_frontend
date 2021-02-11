a92c80247a67cb0dcadd432220a5453a52288b6f
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-02-11 09:31:18 +0900

    カードコンポーネントの追加

---
+++ components/others/Card.vue
@@ -0,0 +1,34 @@
+<template>
+  <article class="card">
+    <div
+      class="thumbnail"
+      :style="{ backgroundImage: 'url(' + thumbnail + ')' }"
+    ></div>
+    <h1>{{ title }}</h1>
+    <p>{{ previewText }}</p>
+  </article>
+</template>
+
+<script>
+export default {
+  props: { thumbnail: String, title: String, previewText: String },
+}
+</script>
+
+<style scoped>
+.card {
+  box-sizing: border-box;
+  width: 280px;
+  padding: 8px;
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #aaa;
+  margin: 10px;
+}
+
+.thumbnail {
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  height: 200px;
+}
+</style>
